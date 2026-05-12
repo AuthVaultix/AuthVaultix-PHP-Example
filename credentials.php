daea399ee6fea09f5e3d79035329ff5216f3343c
lufy <tecfaisal8@gmail.com>
2026-05-12 02:55:07 +0000

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,5 +1,22 @@
 <?php
-$name = "";
-$ownerid = "";
-$secret = "";
-$version = "";
+$envFilePath = __DIR__ . '/.env';
+
+if (file_exists($envFilePath)) {
+    $lines = file($envFilePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) continue;
+
+        if (strpos($line, '=') !== false) {
+            list($key, $val) = explode('=', $line, 2);
+            putenv(trim($key) . "=" . trim($val));
+        }
+    }
+}
+
+$name = getenv('$name');
+$ownerid = getenv('$ownerid');
+$secret = getenv('$secret');
+$version = getenv('$version');
+
+?>
